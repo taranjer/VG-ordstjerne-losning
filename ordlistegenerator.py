81dce6923c623a143ca2f0d29244bac25fb61881
Taran <ruge.taran@gmail.com>
2022-01-15 16:36:06 +0100

    Lagt til kommentar

--- ordlistegenerator.py
+++ ordlistegenerator.py
@@ -1,16 +1,27 @@
 # -*- coding: UTF-8 -*-
+
+"""
+Bruker fullformslista fra Språkbanken, og oppretter en ny fil som inneholder alle ord som består av fire bokstaver eller mer.
+
+Eksempel på linje i fullformslista: (linje 27)
+25	67750	syttende	adj <ordenstall> pos be ent normert	515	3	1996.01.01	4000	normert
+
+Kilde:
+https://www.nb.no/sprakbanken/ressurskatalog/oai-nb-no-sbr-5/
+
+"""
 import re
 
-f = open('fullformsliste.txt',"rt") 
-f.readline()
+fullformsliste = open('fullformsliste.txt',"rt") 
+fullformsliste.readline()
 ordlistefil = open("ordliste.txt", "a")
 regex = re.compile("^[a-zæøå]*$")
 
-while f:
-    linje = str(f.readline())
+while fullformsliste:
+    linje = str(fullformsliste.readline())
     if linje == '':
         break
-    gyldigord = linje.split()[2]
-    if regex.match(gyldigord) and len(gyldigord)>3:
+    norsk_ord = linje.split()[2]
+    if regex.match(norsk_ord) and len(norsk_ord)>3:
         ordlistefil.write(linje.split()[2]+" ")
 
